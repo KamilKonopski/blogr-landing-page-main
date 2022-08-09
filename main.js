e307f264956419a3ff1b62a007530d7c753e2631
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-08-09 22:04:23 +0200

    add mobile menu fuctions

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 const burgerMenu = document.querySelector('.header__image--burger');
 const burgerIcon = document.querySelector('.header__image--burger img');
 const mobileMenu = document.querySelector('.header__nav');
-const menuOptions = document.querySelectorAll('.header__nav--main p');
-
-console.log(menuOptions)
-
-
+const productOption = document.getElementById('product');
+const companyOption = document.getElementById('company');
+const connectOption = document.getElementById('connect');
 
 const showMenu = () => {
     mobileMenu.classList.toggle('show-menu');
@@ -18,3 +16,35 @@ const showMenu = () => {
 }
 
 burgerMenu.addEventListener('click', showMenu)
+
+
+
+
+productOption.addEventListener('click', () => {
+    document.querySelector('.header__nav--product').classList.toggle('show-submenu');
+    if(document.querySelector('.header__nav--product').classList.contains('show-submenu')) {
+        document.querySelector('#product img').style.transform = "rotate(180deg)";
+    } else {
+        document.querySelector('#product img').style.transform = "rotate(0)"
+    }
+})
+
+companyOption.addEventListener('click', () => {
+    document.querySelector('.header__nav--company').classList.toggle('show-submenu');
+    if(document.querySelector('.header__nav--company').classList.contains('show-submenu')) {
+        document.querySelector('#company img').style.transform = "rotate(180deg)";
+    } else {
+        document.querySelector('#company img').style.transform = "rotate(0)"
+    }
+})
+
+connectOption.addEventListener('click', () => {
+    document.querySelector('.header__nav--connect').classList.toggle('show-submenu');
+    if(document.querySelector('.header__nav--connect').classList.contains('show-submenu')) {
+        document.querySelector('#connect img').style.transform = "rotate(180deg)";
+    } else {
+        document.querySelector('#connect img').style.transform = "rotate(0)"
+    }
+})
+
+
